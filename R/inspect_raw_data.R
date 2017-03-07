6cb322b3701d2c0c6b61fd07d0e881e911b7cafc
Guillaume Gaullier <guillaume@gaullier.org>
2017-03-06 22:27:58 -0700

    inspect_raw_data: added mean value and +/- 10 % shaded area in donor plot; closes #1

--- R/inspect_raw_data.R
+++ R/inspect_raw_data.R
@@ -11,6 +11,9 @@
 #'     different symbols: simply add in the \code{Content} column, for each row,
 #'     a word describing which data series or replicate this row belongs to
 #'     (like \code{control_1}, \code{titration_1}, etc.).
+#' @param blank An optional character vector containing the names associated to
+#'     the blank titrations. If not specified, the donor channel plot will not
+#'     display the mean and +/- 10 % shaded area.
 #' @return A list containing three \code{ggplot2} graph objects (named
 #'     \code{donor}, \code{acceptor} and \code{fret}). Warning messages appear
 #'     when missing values are encountered, and can be safely ignored.
@@ -21,11 +24,23 @@
 #' my_plots$donor # Send only donor channel plot to the output device.
 #' @export
 
-inspect_raw_data <- function(raw_data) {
+inspect_raw_data <- function(raw_data, blank = NULL) {
+    if(!is.null(blank) && class(blank) != "character"){
+        stop("Invalid blank names. The 'blank' argument should be a character vector.")
+    }
+
     donor_plot <- ggplot2::ggplot(data = raw_data,
                                   ggplot2::aes(x = concentration,
                                                y = donor_channel))
     donor_plot <- donor_plot + ggplot2::geom_point(ggplot2::aes(shape = Content))
+    if(!is.null(blank)){
+        donor_average <- mean(raw_data$donor_channel[!(raw_data$Content %in% blank)],
+                              na.rm = TRUE)
+        donor_plot <- donor_plot + ggplot2::geom_hline(yintercept = donor_average)
+        donor_plot <- donor_plot + ggplot2::geom_ribbon(ggplot2::aes(ymin = donor_average - donor_average * 10 / 100,
+                                                                     ymax = donor_average + donor_average * 10 / 100),
+                                                        alpha = "0.3")
+    }
     donor_plot <- donor_plot + ggplot2::theme_bw()
     donor_plot <- donor_plot + ggplot2::scale_x_log10()
 
