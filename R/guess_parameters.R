ca8d90eda6f12eec99ad9aeead1dd7d13bd15431
Guillaume Gaullier <guillaume@gaullier.org>
2017-05-23 17:52:26 -0600

    Minor modification to guess_parameters

--- R/guess_parameters.R
+++ R/guess_parameters.R
@@ -1,25 +1,25 @@
 #' @title Guess initial values for parameters of the binding model equations
 #'
 #' @description This function guesses initial values for \code{kd},
-#'     \code{fret_min} and \code{fret_max} (the parameters of the binding model
-#'     equations), using the corrected FRET data.
+#'     \code{signal_min} and \code{signal_max} (the parameters of the binding
+#'     model equations), using the corrected FRET data.
 #'
 #' @param fret_corr A dataframe containing the corrected FRET signal. It must
 #'     contain at least two columns named \code{fret_corrected} and
 #'     \code{concentration}.
-#' @return A list containing initial guesses for \code{kd}, \code{fret_min}
-#'     and \code{fret_max}.
+#' @return A list containing initial guesses for \code{kd}, \code{signal_min}
+#'     and \code{signal_max}.
 #' @export
 
 guess_parameters <- function(fret_corr) {
     # Minimal value of measured FRET signal is a good approximation
     # for signa_min
-    fret_min_guess <- min(fret_corr$fret_corrected)
+    signal_min_guess <- min(fret_corr$fret_corrected)
 
     # Maximal value of measured FRET signal is not a perfect approximation for
     # signal_max (because experimental curves rarely saturate), but good enough
     # for an initial guess
-    fret_max_guess <- max(fret_corr$fret_corrected)
+    signal_max_guess <- max(fret_corr$fret_corrected)
 
     # As an initial guess of kd, we simply take the concentration value at
     # half-maximum FRET signal
@@ -31,6 +31,6 @@ guess_parameters <- function(fret_corr) {
     kd_guess <- fret_corr$concentration[half_fret_measured_index]
 
     list(kd       = kd_guess,
-         fret_min = fret_min_guess,
-         fret_max = fret_max_guess)
+         signal_min = signal_min_guess,
+         signal_max = signal_max_guess)
 }
