3f3d745373769a58d5ecde46624e9b1e30592e5d
Guillaume Gaullier <guillaume@gaullier.org>
2017-05-23 12:18:32 -0600

    Dropped Hill coefficient in the quadratic equation (not sure whether it is valid in this equation)

--- R/equations.R
+++ R/equations.R
@@ -1,11 +1,13 @@
 #' @title Michaelis-Menten equation
 #'
 #' @description This function sets up the Michaelis-Menten equation for use by
-#'     \code{\link{fit_binding_model}} or \code{\link[stats]{nls}}.
+#'     \code{\link{fit_binding_model}} or \code{\link[stats]{nls}}. If a Hill
+#'     coefficient different from 1 is specified, this equation describes a
+#'     Hill model.
 #'
 #' @param titrated_conc Concentrations in the titration series.
 #' @param kd Binding constant.
-#' @param n Hill coefficient.
+#' @param n Hill coefficient. Defaults to 1.
 #' @param signal_min Minimal observed signal.
 #' @param signal_max Maximal observed signal.
 #' @param probe_conc Fixed concentration of probe.
@@ -13,7 +15,7 @@
 
 hyperbola <- function(titrated_conc,
                       kd,
-                      n,
+                      n = 1,
                       signal_min,
                       signal_max,
                       probe_conc) {
@@ -28,7 +30,6 @@ hyperbola <- function(titrated_conc,
 #'
 #' @param titrated_conc Concentrations in the titration series.
 #' @param kd Binding constant.
-#' @param n Hill coefficient.
 #' @param signal_min Minimal observed signal.
 #' @param signal_max Maximal observed signal.
 #' @param probe_conc Fixed concentration of probe.
@@ -41,9 +42,9 @@ quadratic <- function(titrated_conc,
                       signal_max,
                       probe_conc) {
     signal_min + (signal_max - signal_min) *
-        ( (kd + probe_conc + titrated_conc^n) -
-              sqrt( (-(kd + probe_conc + titrated_conc^n))^2 -
-                        4 * probe_conc * titrated_conc^n ) ) / (2 * probe_conc)
+        ( (kd + probe_conc + titrated_conc) -
+              sqrt( (-(kd + probe_conc + titrated_conc))^2 -
+                        4 * probe_conc * titrated_conc ) ) / (2 * probe_conc)
 }
 
 #' @title Quadratic homodimer equation
@@ -53,7 +54,6 @@ quadratic <- function(titrated_conc,
 #'
 #' @param titrated_conc Concentrations in the titration series.
 #' @param kd Binding constant.
-#' @param n Hill coefficient.
 #' @param signal_min Minimal observed signal.
 #' @param signal_max Maximal observed signal.
 #' @param probe_conc Fixed concentration of probe.
