83ee5f3de90d18f36b5bca571293da379c2a4005
Guillaume Gaullier <guillaume@gaullier.org>
2017-05-24 15:09:31 -0600

    Added namespaces for functions from external packages

--- R/average_technical_replicates.R
+++ R/average_technical_replicates.R
@@ -4,15 +4,15 @@
 #'     an arbitrary number of technical replicates.
 #'
 #' @param raw_data A dataframe containing the raw fluorescence data. It must
-#'     contain at least 8 columns named 
+#'     contain at least 8 columns named
 #'     - \code{Experiment}: A unique string identifier for each experiment
 #'     - \code{Type}: Either "blank" (no donor) or "titration" (donor present)
-#'     - \code{Replicate}: An integer that indicates the technical replicate 
-#'     number (ie, 1, 2,...) 
-#'     - \code{Observation}: An integer that identifies each observation 
+#'     - \code{Replicate}: An integer that indicates the technical replicate
+#'     number (ie, 1, 2,...)
+#'     - \code{Observation}: An integer that identifies each observation
 #'     within a run. For a plate-reader based assay, this is usually the row
-#'     number of a well. The number of observations for a titration and a 
-#'     blank must match, and a given \code{Observation} value must have the 
+#'     number of a well. The number of observations for a titration and a
+#'     blank must match, and a given \code{Observation} value must have the
 #'     same ligand concentration for the titration and the blank.
 #'     - \code{concentration}: Ligand concentration
 #'     - \code{donor_channel}: Donor fluorescence
@@ -20,14 +20,14 @@
 #'     - \code{fret_channel}: FRET signal
 #'
 #' @return A dataframe containing the reduced dataset after averaging across
-#'     replicates. It contains all of the above columns *except* 
+#'     replicates. It contains all of the above columns *except*
 #'     \code{Replicate} (becuase we average over replicates)
 #' @export
 
 average_technical_replicates <- function(raw_data) {
-  raw_data %>% 
-    group_by(Experiment, Type, Observation, concentration) %>%
-    summarise(fret = mean(fret_channel), 
-              donor = mean(donor_channel),
-              acceptor = mean(acceptor_channel))
+  raw_data %>%
+    dplyr::group_by(Experiment, Type, Observation, concentration) %>%
+    dplyr::summarise(fret = mean(fret_channel),
+                     donor = mean(donor_channel),
+                     acceptor = mean(acceptor_channel))
 }
--- R/extract_fit_parameters.R
+++ R/extract_fit_parameters.R
@@ -3,16 +3,16 @@
 #' @description This function extracts best fit parameter estimates and standard
 #'     errors from the output of \code{\link{fit_binding_model}}
 #'
-#' @param fits The output of the \code{\link{fit_binding_model}} 
+#' @param fits The output of the \code{\link{fit_binding_model}}
 #' @return A data frame with parameter names, parameter estimates and standard
 #'     errors.
 #' @export
 
 extract_fit_parameters = function(fit.output){
-  fit.results = 
+  fit.results =
     fit.output %>%
-    tidy(fit) %>% 
-    select(Experiment, term, estimate, std.error)
+    broom::tidy(fit) %>%
+    dplyr::select(Experiment, term, estimate, std.error)
   colnames(fit.results) = c("Experiment", "parameter", "estimate", "std.err")
   return(fit.results)
 }
