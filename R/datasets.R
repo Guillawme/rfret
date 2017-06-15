131de8fac3f2ea2bb8e23bd002487147dc452a9a
Guillaume Gaullier <guillaume@gaullier.org>
2017-06-15 17:09:19 -0600

    New example dataset, got rid of the old ones

--- R/datasets.R
+++ R/datasets.R
@@ -1,151 +1,29 @@
-#' A good FRET binding assay dataset
+#' A FRET binding assay dataset
 #'
-#' This dataset was collected on March 16th, 2017. It is a protein-protein
-#' interaction assay, where one of the two proteins is labeled with Alexa 488
-#' (donor fluorophore) and the other with Atto 647 (acceptor fluorophore). This
-#' dataset yields a binding curve and a binding constant of 48 +/- 6.
+#' This dataset was collected on June 2nd, 2017. It is a protein-DNA interaction
+#' assay, where the protein is labeled with a donor fluorophore and the DNA with
+#' an acceptor fluorophore. This dataset yields a binding constant of 4.3 +/-
+#' 0.5 (using the hyperbolic model).
 #'
-#' @format A data frame with 96 rows and 7 columns. Rows contain two replicates
-#' of two experiments each containing 24 data points. Columns are:
+#' @format A data frame with 144 rows and 7 columns. Rows contain two replicates
+#' of three titrations series, each containing 24 data points. Columns are:
 #' \describe{
-#'   \item{Well.Row}{The row where the sample corresponding to this record was
+#'   \item{Well Row}{The row where the sample corresponding to this record was
 #'   located into the 384-well microplate.}
-#'   \item{Well.Col}{The column where the sample corresponding to this record
+#'   \item{Well Col}{The column where the sample corresponding to this record
 #'   was located into the 384-well microplate.}
 #'   \item{Content}{A word describing the experiment and replicate this
-#'   particular data point belongs to (\code{blank_1}, \code{blank_2},
-#'   \code{titration_1}, \code{titration_2}).}
+#'   particular data point belongs to (\code{acceptor_only.1},
+#'   \code{acceptor_only.2}, \code{donor_only.1}, \code{donor_only.2}
+#'   \code{titration.1}, \code{titration.2}).}
 #'   \item{fret_channel}{The fluorescence intensities measured in the FRET
 #'   channel.}
 #'   \item{acceptor_channel}{The fluorescence intensities measured in the
 #'   acceptor channel.}
 #'   \item{donor_channel}{The fluorescence intensities measured in the donor
 #'   channel.}
-#'   \item{concentration}{The concentrations of acceptor-labeled protein in the
-#'   titration series.}
+#'   \item{concentration}{The concentrations of macromolecule in the titration
+#'   series.}
 #' }
 #' @source Guillaume Gaullier
-"fret_good"
-
-#' A FRET binding assay dataset with outliers in the donor channel
-#'
-#' This dataset was collected on February 24th, 2017. It is a protein-protein
-#' interaction assay, where one of the two proteins is labeled with Alexa 488
-#' (donor fluorophore) and the other with Atto 647 (acceptor fluorophore). This
-#' dataset has outliers in the donor channel, but can still yield a binding
-#' curve and a binding constant of 147 +/- 41.
-#'
-#' @format A data frame with 96 rows and 7 columns. Rows contain two replicates
-#' of two experiments each containing 24 data points. Columns are:
-#' \describe{
-#'   \item{Well.Row}{The row where the sample corresponding to this record was
-#'   located into the 384-well microplate.}
-#'   \item{Well.Col}{The column where the sample corresponding to this record
-#'   was located into the 384-well microplate.}
-#'   \item{Content}{A word describing the experiment and replicate this
-#'   particular data point belongs to (\code{blank_1}, \code{blank_2},
-#'   \code{titration_1}, \code{titration_2}).}
-#'   \item{fret_channel}{The fluorescence intensities measured in the FRET
-#'   channel.}
-#'   \item{acceptor_channel}{The fluorescence intensities measured in the
-#'   acceptor channel.}
-#'   \item{donor_channel}{The fluorescence intensities measured in the donor
-#'   channel.}
-#'   \item{concentration}{The concentrations of acceptor-labeled protein in the
-#'   titration series.}
-#' }
-#' @source Guillaume Gaullier
-"fret_donor_outliers"
-
-#' A FRET binding assay dataset with outliers in the acceptor channel
-#'
-#' This dataset was collected on March 13th, 2017. It is a protein-protein
-#' interaction assay, where one of the two proteins is labeled with Alexa 488
-#' (donor fluorophore) and the other with Atto 647 (acceptor fluorophore). This
-#' dataset has outliers in the acceptor channel, but can still yield a binding
-#' curve and a binding constant of 63 +/- 10.
-#'
-#' @format A data frame with 96 rows and 7 columns. Rows contain two replicates
-#' of two experiments each containing 24 data points. Columns are:
-#' \describe{
-#'   \item{Well.Row}{The row where the sample corresponding to this record was
-#'   located into the 384-well microplate.}
-#'   \item{Well.Col}{The column where the sample corresponding to this record
-#'   was located into the 384-well microplate.}
-#'   \item{Content}{A word describing the experiment and replicate this
-#'   particular data point belongs to (\code{blank_1}, \code{blank_2},
-#'   \code{titration_1}, \code{titration_2}).}
-#'   \item{fret_channel}{The fluorescence intensities measured in the FRET
-#'   channel.}
-#'   \item{acceptor_channel}{The fluorescence intensities measured in the
-#'   acceptor channel.}
-#'   \item{donor_channel}{The fluorescence intensities measured in the donor
-#'   channel.}
-#'   \item{concentration}{The concentrations of acceptor-labeled protein in the
-#'   titration series.}
-#' }
-#' @source Guillaume Gaullier
-"fret_acceptor_outliers"
-
-#' A bad FRET binding assay dataset
-#'
-#' This dataset was collected on March 16th, 2017. It is a protein-protein
-#' interaction assay, where one of the two proteins is labeled with Alexa 488
-#' (donor fluorophore) and the other with Atto 647 (acceptor fluorophore). This
-#' dataset doesn't yield any binding curve nor binding constant. The reasons for
-#' that are because the concentrations of donor- and acceptor-labeled proteins
-#' are not what they should be (possibly due to protein agregation, or proteins
-#' sticking to the microplate plastic), and the microplate used has an
-#' unusually high auto-fluorescence background in the donor and FRET channels.
-#'
-#' @format A data frame with 96 rows and 7 columns. Rows contain two replicates
-#' of two experiments each containing 24 data points. Columns are:
-#' \describe{
-#'   \item{Well.Row}{The row where the sample corresponding to this record was
-#'   located into the 384-well microplate.}
-#'   \item{Well.Col}{The column where the sample corresponding to this record
-#'   was located into the 384-well microplate.}
-#'   \item{Content}{A word describing the experiment and replicate this
-#'   particular data point belongs to (\code{blank_1}, \code{blank_2},
-#'   \code{titration_1}, \code{titration_2}).}
-#'   \item{fret_channel}{The fluorescence intensities measured in the FRET
-#'   channel.}
-#'   \item{acceptor_channel}{The fluorescence intensities measured in the
-#'   acceptor channel.}
-#'   \item{donor_channel}{The fluorescence intensities measured in the donor
-#'   channel.}
-#'   \item{concentration}{The concentrations of acceptor-labeled protein in the
-#'   titration series.}
-#' }
-#' @source Guillaume Gaullier
-"fret_bad"
-
-#' A FRET binding assay dataset with saturated reads
-#'
-#' This dataset was collected on March 13th, 2017. It is a protein-protein
-#' interaction assay, where one of the two proteins is labeled with Alexa 488
-#' (donor fluorophore) and the other with Atto 647 (acceptor fluorophore). This
-#' dataset has a couple saturated reads (fluorescence intensity of 260000), but
-#' can still yield a binding curve and a binding constant of 124 +/- 18.
-#'
-#' @format A data frame with 96 rows and 7 columns. Rows contain two replicates
-#' of two experiments each containing 24 data points. Columns are:
-#' \describe{
-#'   \item{Well.Row}{The row where the sample corresponding to this record was
-#'   located into the 384-well microplate.}
-#'   \item{Well.Col}{The column where the sample corresponding to this record
-#'   was located into the 384-well microplate.}
-#'   \item{Content}{A word describing the experiment and replicate this
-#'   particular data point belongs to (\code{blank_1}, \code{blank_2},
-#'   \code{titration_1}, \code{titration_2}).}
-#'   \item{fret_channel}{The fluorescence intensities measured in the FRET
-#'   channel.}
-#'   \item{acceptor_channel}{The fluorescence intensities measured in the
-#'   acceptor channel.}
-#'   \item{donor_channel}{The fluorescence intensities measured in the donor
-#'   channel.}
-#'   \item{concentration}{The concentrations of acceptor-labeled protein in the
-#'   titration series.}
-#' }
-#' @source Guillaume Gaullier
-"fret_saturated_reads"
+"fret_data"
