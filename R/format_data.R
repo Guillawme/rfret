8cfe945e78bf8bbd316bcfaa0a76b89932ede58d
Guillaume Gaullier <guillaume@gaullier.org>
2017-06-15 14:20:46 -0600

    format_data finally handles any kind of input (directory name, file names, df)

--- R/format_data.R
+++ R/format_data.R
@@ -1,19 +1,19 @@
-#' @title Format experimental data for subsequent processing
+#' @title Format raw experimental FRET binding data for subsequent processing
 #'
 #' @description This function formats data for subsequent processing by
 #'     \code{\link{average_technical_replicates}}. It will accept all data files
 #'     in a given directory, or any number of data files specified by their
-#'     names, or any number of dataframes supplied as a named list.
+#'     names, or any number of dataframes supplied as a named list, or even a
+#'     single dataframe supplied as is.
 #' @param input A directory name, a vector of file names, a single dataset
 #'     already loaded in memory as a dataframe, or a list of datasets already
 #'     loaded in memory as dataframes.
 #' @param skip_lines The number of lines to skip at the beginning of CSV files
 #'     (usually containing header information before the actual data starts).
-#' @return A single dataframe with the combined input data, containing 11
+#' @return A single dataframe with the combined input data, containing 8
 #'     columns: \code{Experiment}, \code{Type}, \code{Replicate},
-#'     \code{Observation}, \code{Well Row}, \code{Well Col}, \code{Content},
-#'     \code{fret_channel}, \code{acceptor_channel}, \code{donor_channel} and
-#'     \code{concentration}.
+#'     \code{Observation}, \code{fret_channel}, \code{acceptor_channel},
+#'     \code{donor_channel} and \code{concentration}.
 #' @examples
 #' \dontrun{
 #' format_data("my_data_directory", 4)
@@ -21,26 +21,22 @@
 #' @export
 
 format_data <- function(input = NULL, skip_lines = 0) {
-    # Check the input type to determine whether we need to load datafiles from
-    # files.
-    # If no input is provided, ask for one:
+    # Check the input type to determine how to access the data
     if (is.null(input)) {
+        # If no input is provided, ask for one
         stop("You must specify an input: a directory name, a vector of file names, a dataframe, or a list of dataframes.")
-    }
-    # If a single dataframe is provided, we put it in a named list so mapply can
-    # process it:
-    if (is.data.frame(input)) {
-        data_name <- deparse(substitute(input))
-        raw_data <- list(data_name = input)
-    }
-    # If a list if provided, we will assume it's a list of already loaded
-    # datasets:
-    if (is.list(input)) {
+    } else if (is.list(input) && is.data.frame(input)) {
+        # If a single dataframe is provided, we put it in a named list so mapply
+        # can process it
+        raw_data <- list(input)
+        names(raw_data) <- deparse(substitute(input))
+    } else if (is.list(input) && !is.data.frame(input)) {
+        # If we receive a list, we will assume it is a list of dataframes
+        # corresponding to already loaded datasets
         raw_data <- input
-    }
-    # If a character is provided, try to find out whether it's a directory name
-    # or vector of file names, and load files accordingly:
-    if (is.character(input) && length(input) == 1) {
+    } else if (is.character(input) && length(input) == 1) {
+        # If we receive a character, try to figure out whether it is the name
+        # of a directory or a vector of file names
         if (dir.exists(input)) {
             files <- list.files(pattern = ".csv")
             raw_data <- lapply(files, readr::read_csv, skip = skip_lines)
@@ -56,6 +52,8 @@ format_data <- function(input = NULL, skip_lines = 0) {
             stop("File or directory not found: ", input)
         }
     } else if (is.character(input) && length(input) > 1) {
+        # If we receive a character vector longer than 1, it has to be a set of
+        # fine names
         if (FALSE %in% file.exists(input)) {
             stop("File not found: ", input[file.exists(input) == FALSE])
         } else {
@@ -65,6 +63,7 @@ format_data <- function(input = NULL, skip_lines = 0) {
                                    replacement = "")
         }
     } else {
+        # If the input doesn't match any of the above, complain!
         stop("Please provide a valid directory name, or valid file names.")
     }
 
@@ -82,11 +81,10 @@ format_data <- function(input = NULL, skip_lines = 0) {
 #'     \code{\link{average_technical_replicates}} and
 #'     \code{\link{correct_fret_signal}}.
 #' @param raw_data A single dataframe to process.
-#' @param experiment_name Name of the corresponding experiment.
-#' @return A dataframe containing 11 columns: \code{Experiment}, \code{Type},
-#'     \code{Replicate}, \code{Observation}, \code{Well Row}, \code{Well Col},
-#'     \code{Content}, \code{fret_channel}, \code{acceptor_channel},
-#'     \code{donor_channel} and \code{concentration}.
+#' @param experiment_name The name of the corresponding experiment.
+#' @return A dataframe containing 8 columns: \code{Experiment}, \code{Type},
+#'     \code{Replicate}, \code{Observation}, \code{fret_channel},
+#'     \code{acceptor_channel}, \code{donor_channel} and \code{concentration}.
 #' @examples
 #' \dontrun{
 #' format_one_dataset(my_data, "my_experiment")
@@ -100,7 +98,9 @@ format_one_dataset <- function(raw_data, experiment_name) {
     # Generate columns Type and Replicate, based on column Content
     raw_data %<>%
         dplyr::rowwise() %>%
-        dplyr::mutate(intermediate = strsplit(Content, "_"),
+        dplyr::mutate(intermediate = strsplit(Content,
+                                              split = ".",
+                                              fixed = TRUE),
                       Type = intermediate[1],
                       Replicate = intermediate[2]) %>%
         dplyr::select(-intermediate)
@@ -113,15 +113,12 @@ format_one_dataset <- function(raw_data, experiment_name) {
                           dplyr::row_number(dplyr::desc(concentration)))
 
     # Reorder columns and return dataset
-    raw_data <- raw_data[c("Experiment",
-                           "Type",
-                           "Replicate",
-                           "Observation",
-                           "Well Row",
-                           "Well Col",
-                           "Content",
-                           "fret_channel",
-                           "acceptor_channel",
-                           "donor_channel",
-                           "concentration")]
+    raw_data[c("Experiment",
+               "Type",
+               "Replicate",
+               "Observation",
+               "fret_channel",
+               "acceptor_channel",
+               "donor_channel",
+               "concentration")]
 }
