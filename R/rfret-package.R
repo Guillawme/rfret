85cdef24289c50994a016f497d924aac62ef1c57
Guillaume Gaullier <guillaume@gaullier.org>
2017-07-27 16:37:16 -0600

    Added package top level documentation

---
+++ R/rfret-package.R
@@ -0,0 +1,13 @@
+#' rfret: Analyze FRET binding data
+#'
+#' The rfret package provides functions to perform all steps of the analysis of
+#' FRET binding data: inspection of raw data, averaging of technical replicates,
+#' correction of the FRET signal using signal from a blank experiment, fitting
+#' of a binding model equation to the experimental data to determine the binding
+#' constant, making a final figure, and performing analysis in batch processing
+#' mode.
+#'
+#' @name rfret
+#' @docType package
+
+NULL
