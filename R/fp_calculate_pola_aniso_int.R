888e6ef1baa9935f8b0155461bdd133a346617dc
Guillaume Gaullier <guillaume@gaullier.org>
2017-11-12 16:26:44 -0700

    Added FP analysis pipeline, with column name abstraction; see #31 and #35

---
+++ R/fp_calculate_pola_aniso_int.R
@@ -0,0 +1,43 @@
+#' @title Calculate fluorescence polarization and anisotropy from raw data
+#'
+#' @description This function calculates fluorescence polarization and
+#'     anisotropy values from the raw data of parallel and perpendicularly
+#'     polarized channels.
+#'
+#' @param raw_data A dataframe containing the raw data. It must contain at least
+#'     7 columns named:
+#'     \describe{
+#'     \item{Experiment}{A unique name identifying each experiment.}
+#'     \item{Type}{For example, "titration".}
+#'     \item{Replicate}{A number identifying the technical replicate (1, 2, etc.).}
+#'     \item{Observation}{A number identifying each observation in a titration
+#'     series (corresponds to the plate column numbers, if experiments are set
+#'     up as rows in a 384-well plate). The number of observations for an
+#'     experiment and its blanks must match, and a given observation number must
+#'     associate data points at the same concentration in the titration series.}
+#'     \item{concentration}{The ligand concentration in the titration series.}
+#'     \item{\code{parallel}}{The name of the column containing fluorescence
+#'     intensities from the parallel channel.}
+#'     \item{\code{perpendicular}}{The name of the column containing fluorescence
+#'     intensities from the perpendicular channel.}
+#'     }
+#'     The output of \code{\link{fp_format_data}} can be used directly as input
+#'     for this function.
+#' @return A dataframe containing the same columns as the input dataframe, plus
+#'     three columns called \code{polarization}, \code{anisotropy} and
+#'     \code{intensity}.
+#'
+#' @seealso \code{\link{fp_format_data}} to prepare datasets for use with
+#'     \code{fp_calculate_pola_aniso_int}.
+#'
+#' @importFrom magrittr %>%
+#'
+#' @export
+
+fp_calculate_pola_aniso_int <- function(raw_data) {
+    raw_data %>%
+        dplyr::group_by(Experiment, Type, Observation, concentration) %>%
+        dplyr::mutate(polarization = (parallel - perpendicular) / (parallel + perpendicular),
+                      anisotropy   = (parallel - perpendicular) / (parallel + 2 * perpendicular),
+                      intensity    = parallel + 2 * perpendicular)
+}
--- R/fp_format_data.R
+++ R/fp_format_data.R
@@ -10,10 +10,39 @@
 #'     loaded in memory as dataframes.
 #' @param skip_lines The number of lines to skip at the beginning of CSV files
 #'     (usually containing header information before the actual data starts).
+#' @param metadata_json A JSON file describing mappings between internal
+#'     names and metadata information in the actual data files. It can contain
+#'     the following entries:
+#'     \describe{
+#'     \item{\code{content}}{The name of the column describing sample content.
+#'     Default value: \code{"Content"}.}
+#'     \item{\code{concentration}}{The name of the column containing the
+#'     concentration series. Default value: \code{"concentration"}.}
+#'     \item{\code{parallel}}{The name of the column containing fluorescence
+#'     intensities from the parallel channel. Default value: \code{"parallel"}.}
+#'     \item{\code{perpendicular}}{The name of the column containing
+#'     fluorescence intensities from the perpendicular channel. Default value:
+#'     \code{"perpendicular"}.}
+#'     \item{\code{polarization}}{The name of the column containing fluorescence
+#'     polarization values, if already calculated by the instrument. Default
+#'     value: \code{"polarization"}.}
+#'     \item{\code{anisotropy}}{The name of the column containing fluorescence
+#'     anisotropy values, if already calculated by the instrument. Default
+#'     value: \code{"anisotropy"}.}
+#'     \item{\code{intensity}}{The name of the column containing total
+#'     fluorescence intensity values, if already calculated by the instrument.
+#'     Default value: \code{"intensity"}.}
+#'     \item{\code{titration}}{The name describing points of the titration
+#'     series. Default value: \code{"titration"}.}
+#'     \item{\code{baseline}}{The name describing points of the baseline series
+#'     (fluorescent probe only, without titrant molecule). Default value:
+#'     \code{"baseline"}.}
+#'     \item{\code{buffer_only}}{The name describing points of the control
+#'     series with only buffer. Default value: \code{"buffer_only"}.}}
 #' @return A single dataframe with the combined input data, containing 8
 #'     columns: \code{Experiment}, \code{Type}, \code{Replicate},
-#'     \code{Observation}, \code{Polarization}, \code{Anisotropy},
-#'     \code{Intensity} and \code{concentration}. Rows with missing values
+#'     \code{Observation}, \code{polarization}, \code{anisotropy},
+#'     \code{intensity} and \code{concentration}. Rows with missing values
 #'     will be dropped.
 #' @examples
 #' \dontrun{
@@ -24,7 +53,17 @@
 #'
 #' @export
 
-fp_format_data <- function(input = NULL, skip_lines = 0) {
+fp_format_data <- function(input = NULL,
+                           skip_lines = 0,
+                           metadata_json = NULL) {
+    # Check if we need to read user-provided metadata (otherwise, defaults will
+    # be used automatically). If we use non-default metadata, reset to default
+    # after the function terminates.
+    if(!is.null(metadata_json)) {
+        default <- get_user_metadata(metadata_json)
+        on.exit(.rfret$metadata <- default)
+    }
+
     # Load data
     raw_data <- load_data(input, skip_lines)
 
@@ -47,8 +86,8 @@ fp_format_data <- function(input = NULL, skip_lines = 0) {
 #' @param raw_data A single dataframe to process.
 #' @param experiment_name The name of the corresponding experiment.
 #' @return A dataframe containing 8 columns: \code{Experiment}, \code{Type},
-#'     \code{Replicate}, \code{Observation}, \code{Polarization},
-#'     \code{Anisotropy}, \code{Intensity} and \code{concentration}.
+#'     \code{Replicate}, \code{Observation}, \code{polarization},
+#'     \code{anisotropy}, \code{intensity} and \code{concentration}.
 #' @examples
 #' \dontrun{
 #' fp_format_one_dataset(my_data, "my_experiment")
@@ -57,33 +96,45 @@ fp_format_data <- function(input = NULL, skip_lines = 0) {
 #' @importFrom magrittr %>%
 
 fp_format_one_dataset <- function(raw_data, experiment_name) {
-    # Generate an Experiment column, where the experiment name is its
-    # corresponding file name without the .csv extension
+    # Generate an Experiment column, and retrieve experiment name here
     raw_data$Experiment <- experiment_name
 
+    # Data formatting pipeline:
     # Generate columns Type and Replicate, based on column Content
     raw_data %>%
         dplyr::rowwise() %>%
-        dplyr::mutate(intermediate = strsplit(Content,
-                                              split = ".",
-                                              fixed = TRUE),
+        dplyr::mutate(intermediate = stringr::str_split(rlang::UQ(.rfret$metadata$content),
+                                                        pattern = stringr::fixed(".")),
                       Type = intermediate[1],
                       Replicate = intermediate[2]) %>%
         dplyr::select(-intermediate) %>%
-        # Generate an Observation column, to label observations across different
-        # types of experiments (blank, titration) and replicates
-        dplyr::group_by(Experiment, Type, Replicate) %>%
-        dplyr::mutate(Observation =
-                          dplyr::row_number(dplyr::desc(concentration))) %>%
         dplyr::ungroup() %>%
-        # Reorder columns and return dataset
+    # Generate an Observation column, to label observations across different
+    # types of experiments (baseline and titration) and replicates
+        dplyr::group_by(Experiment, Type, Replicate) %>%
+        dplyr::mutate(Observation = rlang::UQ(.rfret$metadata$concentration) %>%
+                          dplyr::desc() %>%
+                          dplyr::row_number()
+                      ) %>%
+        dplyr::ungroup() %>%
+    # Rename sample type identifyers
+        dplyr::mutate(Type = replace(Type,
+                                     Type == rlang::quo_text(rlang::as_quosure(.rfret$metadata$buffer_only)),
+                                     "buffer_only")) %>%
+        dplyr::mutate(Type = replace(Type,
+                                     Type == rlang::quo_text(rlang::as_quosure(.rfret$metadata$baseline)),
+                                     "baseline")) %>%
+        dplyr::mutate(Type = replace(Type,
+                                     Type == rlang::quo_text(rlang::as_quosure(.rfret$metadata$titration)),
+                                     "titration")) %>%
+    # Reorder columns and return dataset
         dplyr::select(Experiment,
                       Type,
                       Replicate,
                       Observation,
-                      Polarization,
-                      Anisotropy,
-                      Intensity,
-                      concentration) %>%
+                      polarization  = rlang::UQ(.rfret$metadata$polarization),
+                      anisotropy    = rlang::UQ(.rfret$metadata$anisotropy),
+                      intensity     = rlang::UQ(.rfret$metadata$intensity),
+                      concentration = rlang::UQ(.rfret$metadata$concentration)) %>%
         dplyr::mutate(Replicate = as.integer(Replicate))
 }
