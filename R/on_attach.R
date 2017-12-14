817ec916901538e36a9ed5eca1d013d045404ab5
Guillaume Gaullier <guillaume@gaullier.org>
2017-12-14 12:01:49 -0700

    Silence irrelevant 'no visible binding to global variable' notes from R CMD check
    This will be important to get the package accepted into CRAN.

--- R/on_attach.R
+++ R/on_attach.R
@@ -11,3 +11,32 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage("Successfully loaded ", pkgname, " from ", libname)
 }
+
+# Make R CMD check ignore the following symbols.
+# This is to silence "no visible binding to global variable" notes that are
+# irrelevant because these symbols are column names in dataframes.
+utils::globalVariables(names = c(".",
+                                 ".resid",
+                                 "Experiment",
+                                 "Observation",
+                                 "Replicate",
+                                 "Type",
+                                 "acceptor_channel",
+                                 "anisotropy",
+                                 "concentration",
+                                 "donor_channel",
+                                 "estimate",
+                                 "fit",
+                                 "fret_channel",
+                                 "intensity",
+                                 "intermediate",
+                                 "parallel",
+                                 "perpendicular",
+                                 "polarization",
+                                 "signal",
+                                 "status",
+                                 "std.error",
+                                 "term",
+                                 "x"),
+                       package = "rfret",
+                       add = TRUE)
